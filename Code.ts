0c6b402feaac914d02732d8b90cec6c31bcb078e
09rd193 <09rd193@gmail.com>
2019-04-16 18:00:12 +0900

    Update Code.ts

--- Code.ts
+++ Code.ts
@@ -1,8 +1,21 @@
-const greeter = (person: string) => {
-    return `Hello, ${person}!`;
-  }
-  
-  function testGreeter() {
-    const user = 'Grant';
-    Logger.log(greeter(user));
-  }
+function test(): void {
+    var url:string = 'https://hapitas.jp/item/detail/itemid/69141';
+    var url:string = 'https://hapitas.jp/item/detail/itemid/33008';
+    var response:HTTPResponse = UrlFetchApp.fetch(url);
+    var html:string = response.getContentText('UTF-8');
+
+    var itemid:string   = html.match(/<meta name="cXenseParse:ozv-itemid" content="(\d+)" /)[1];
+    var title:string    = html.match(/<meta name="cXenseParse:ozv-title" content="(.+)" /)[1];
+    var category:string = html.match(/<meta name="cXenseParse:ozv-detailcategory" content="(.+)" /)[1];
+    var point:string    = html.match(/<li class="item_point"><strong class="point">(.+)<span class="pointback_unit">ポイントバック/)[1];
+    var banner:string   = html.match(/<span class="banner"><img src="(.+)" /)[1];
+
+    Logger.log(itemid);
+    Logger.log(title);
+    Logger.log(category);
+    point = point.replace('<del>', '').replace('</del>', '').replace('<ins>', ',').replace('</ins>', '').replace('</strong>', '');
+    Logger.log(point);
+    Logger.log(banner);
+    Logger.log(html);
+
+}
